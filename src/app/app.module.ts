0a666aaf6cddecb3e83d7e38f4c00118d24114cc
Yael2407 <edson2gest73@gmail.com>
2021-11-02 02:29:15 -0600

    E2 A4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,9 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AUTOMOVILES } from '../data';
 import { Automovil } from '../models';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list',
@@ -8,11 +9,23 @@ import { Automovil } from '../models';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  autos!: Automovil[];
-  constructor() { }
+  autos: Automovil[] = [];
+  autoSeleccionado?: Automovil;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
     this.autos = AUTOMOVILES;
+  }
+
+  openModal(auto: Automovil, content: any) {
+    this.autoSeleccionado = auto;
+
+    this.modalService.open(content).result.then((result) => {
+      this.autoSeleccionado = undefined;
+    }, (reason) => {
+      this.autoSeleccionado = undefined;
+    });
   }
 
 }
