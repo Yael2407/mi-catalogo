cf8a6d447e7f99d79c9a398b7eca30b10d6c4a65
Yael2407 <edson2gest73@gmail.com>
2021-11-03 17:31:40 -0600

    E3 A6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: '/table', 
+    pathMatch: 'full' 
+  },
   {
     path: 'list',
     component: ListComponent
@@ -11,6 +17,10 @@ const routes: Routes = [
   {
     path: 'table',
     component: TableComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent 
   }
 ];
 
